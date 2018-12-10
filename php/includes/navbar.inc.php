88ef8d2302bf90a1fe951c6ac4cb027d087bca26
Eva <vica.bukli@gmail.com>
2018-12-10 09:39:32 +0100

    friday changes eva

--- php/includes/navbar.inc.php
+++ php/includes/navbar.inc.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Shelter</title>
+    <title>Útulok Piešťany</title>
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -22,34 +22,62 @@
 
   </head>
 
-  <body>
+  <body id="page-top">
 
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
       <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="#page-top">Animal Shelter</a>
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">Útulok Piešťany</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           Menu
           <i class="fas fa-bars"></i>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav text-uppercase ml-auto">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle js-scroll-trigger" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                About us
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="#">Our Mission</a>
+                <a class="dropdown-item" href="#">Our Team</a>
+                <a class="dropdown-item" href="#">Sponsors</a>
+                <a class="dropdown-item" href="#">Gallery</a>
+              </div>
+            </li>
+             <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle js-scroll-trigger" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Dog & Cats
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="#">Dogs</a>
+                <a class="dropdown-item" href="#">Cats</a>
+                <a class="dropdown-item" href="#">Search</a>
+                <a class="dropdown-item" href="#">Adopt</a>
+                <a class="dropdown-item" href="#">Support</a>
+                <a class="dropdown-item" href="#">In Memoriam</a>
+              </div>
+            </li>
             <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#services">Services</a>
+              <a class="nav-link js-scroll-trigger" href="#services">Reports</a>
             </li>
             <li class="nav-item">
               <a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#about">About</a>
+              <a class="nav-link js-scroll-trigger" href="#about">Contact</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#team">Team</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle js-scroll-trigger" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Other
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="#">To Read (blog)</a>
+                <a class="dropdown-item" href="#">Documents</a>
+              </div>
             </li>
           </ul>
         </div>
       </div>
     </nav>
+     <img class="logo" src="../img/logo.png" alt="l" width="50">
---
+++ php/landing.php
@@ -0,0 +1,88 @@
+<?php include("includes/navbar.inc.php"); ?>
+
+<link rel="stylesheet" type="text/css" href="../css/landing.css">
+
+    <!-- Header -->
+    <header class="masthead">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-5 offset-1">
+            <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img class="d-block w-100" src="../img/animal2.jpg">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="../img/dog.jpg">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="../img/cat.jpg">
+                </div>
+                <a class="carousel-control-prev arrow" href="#carouselExampleFade" role="button" data-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next arrow" href="#carouselExampleFade" role="button" data-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Next</span>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-5">
+            <div class="righttext">
+              <h1 class="pb-3">Útulok Piešťany</h1>
+              <button class="btn btn-success mt-3 ">donate</button>
+            </div>
+            
+          </div>
+
+        </div>
+      </div>
+    </header>
+
+<section class="part2">
+     <div class="container">
+       <div class="row">
+        <div class="col-12">
+          <h4>Random dog & cat</h4>
+        </div>
+         <div class="col-lg-3 col-md-6 mb-4 smallbox">
+           <img class="smallpic" src="../img/adorable-animal-breed-1189673.jpg" alt="">
+           <div class="top-left"><span class="imgtext">Bogyó</span></div>
+           <div class="bottomtext">
+             <p>bla bla bla</p>
+           </div>
+         </div>
+         <div class="col-lg-3 col-md-6 mb-4 smallbox">
+           <img class="smallpic" src="../img/animal-cat-cat-face-951336.jpg" alt="">
+           <div class="top-left"><span class="imgtext">Bogyó</span></div>
+           <div class="bottomtext">
+             <p>bla bla bla</p>
+           </div>
+
+         </div>
+         <div class="col-lg-3 col-md-6 mb-4 smallbox">
+           <img class="smallpic" src="../img/adorable-animal-black-and-white-1404727.jpg" alt="">
+           <div class="top-left"><span class="imgtext">Bogyó</span></div>
+          <div class="bottomtext">
+             <p>bla bla bla</p>
+           </div>
+
+         </div>
+         <div class="col-lg-3 col-md-6 mb-4 smallbox">
+           <img class="smallpic" src="../img/sponsor-cat.PNG" alt="">
+           <div class="top-left"><span class="imgtext">Bogyó</span></div>
+            <div class="bottomtext">
+             <p>bla bla bla</p>
+           </div>
+
+         </div>
+       </div>
+     </div>
+</section>   
+ 
+
+<?php include("includes/footer.inc.php"); ?>
