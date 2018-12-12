6b8b2ac9b838f7917a6a647fbbe3634461b1c02f
Nico <nico.adelmann@hotmail.com>
2018-12-12 18:58:03 +0100

    sponsor page2

--- php/gallary.php
+++ php/gallary.php
@@ -1,7 +1,3 @@
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <link rel="stylesheet" type="text/css" href="../css/gallery.css">
 <?php include("includes/navbar.inc.php"); ?>
 <?php include("includes/dbh.inc.php");
@@ -44,18 +40,3 @@ $newarray = $result->fetch_all(MYSQLI_ASSOC);
 <?php } ?>
  </div>
 <?php include("includes/footer.inc.php"); ?>
-
-
-<script>
-	$('.make_bigger').click(function() {
-  $('.active').not(this).addClass('non_active');
-  $('.active').not(this).removeClass('active');
-  if ($(this).hasClass('active')) {
-    $(this).addClass('non_active');
-    $(this).removeClass('active');
-  } else {
-    $(this).removeClass('non_active');
-    $(this).addClass('active');
-  }
-});
-</script>
--- php/sponsors.php
+++ php/sponsors.php
@@ -1,33 +1,45 @@
-<?php
-include("includes/dbh.inc.php");
-include("includes/navbar.inc.php");
-include("includes/output_sponsors.inc.php"); ?>
+<link rel="stylesheet" type="text/css" href="../css/gallery.css">
+<?php include("includes/navbar.inc.php"); ?>
+<?php include("includes/dbh.inc.php");
+$sql= "SELECT * FROM sponsors";
+$result = mysqli_query($conn,$sql);
 
-<link rel="stylesheet" type="text/css" href="../css/sponsors.css">
-<div class="sponsors_content">
-	<h1 class="sponsors_heading">Our Sponsors</h1>
-	<div class="sponsors_description">
-	<img src="../img/sponsor-cat.png" alt="">
-	
-	<p class="sponsors_description_text">Because we are a non-profit organization, our activities would be impossible without voluntary donations and sponsorship gifts. We are grateful for any help and interest expressed. We would also like to publicly express our gratitude to our following sponsors. We would like to thank all who have supported us with donations or gifts in public gatherings or in any other way.</p>
-	
-	</div>
-	<div class="sponsors_sponsor_content">
-		<h3>Special thanks to:</h3>
-	<?php 
-	foreach($sponsorsResult as $row) { ?>
- 	
-		<div class="sponsors_box">
-			<h5><?php echo $row['sponsors_name'] ?></h5>
-			<img src="<?php echo $row['sponsors_logo'] ?>" alt="">
-			<p><?php echo $row['sponsors_email'] ?></p>
-			<p><?php echo $row['sponsors_address'] ?></p>
-		</div>
-	
-<?php }; ?>
+$newarray = $result->fetch_all(MYSQLI_ASSOC);
+?>
 
-	</div>
+<div id='main' class='containerr pb-5 pt-5'>
+
+<?php foreach ($newarray as $key) { ?>
+
+
+		<a data-toggle="modal" data-target="#imageModal<?php echo $key["sponsors_id"];?>"><img src='<?php echo $key['sponsors_logo'] ?>' class='img-thumbnail make_bigger' alt='Cinque Terre'></a>
+
+
+
+ <div class="modal fade" id="imageModal<?php echo $key["sponsors_id"];?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog image_modal modal-md" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Sponsor</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body text-center">
+        <img src='<?php echo $key['sponsors_logo'] ?>' class='img-large' alt='Cinque Terre'>
+        <h5 class="text-center"><?php echo $key['sponsors_name'] ?></h5>
+        <p class="text-center"><?php echo $key['sponsors_email'] ?></p>
+        <p class="text-center"><?php echo $key['sponsors_address'] ?></p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
 </div>
 
 
+
+<?php } ?>
+ </div>
 <?php include("includes/footer.inc.php"); ?>
