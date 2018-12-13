d777184e42d26ce3ef1e0b8e621c88bad346a791
Nico <nico.adelmann@hotmail.com>
2018-12-13 10:52:27 +0100

    fixed navbar once again

--- php/adminpanel/upload_dog.php
+++ php/adminpanel/upload_dog.php
@@ -12,6 +12,7 @@ include("includes_admin/upload_dogs.inc.php"); ?>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body id="page-top">
+  <?php include "includes_admin/navbar.inc.php"; ?>
   <div class="container">
     <div class="row">
       <div class="col-8 offset-2">
